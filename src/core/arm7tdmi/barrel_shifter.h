33466080bb55a5433d7f93e9cf4fff2abf3297d8
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2021-03-29 03:24:38 +0100

    change case ranges to if else (for now)
    using if else chain for now is fine, will optimise it much later on.
    
    reason for change is that case range is not c-standard, and thus msvc does not (sadly) support it

--- src/core/arm7tdmi/barrel_shifter.h
+++ src/core/arm7tdmi/barrel_shifter.h
@@ -17,28 +17,60 @@ struct ShiftResult {
 };
 
 static inline struct ShiftResult barrel_shift_LSL(const uint32_t value, const uint32_t shift_v, const bool old_carry) {
-    switch (shift_v) {
-        case 0:
-            return (struct ShiftResult){ .value = value, .carry = old_carry };
-        case 1 ... 31:
-            return (struct ShiftResult){ .value = value << shift_v, .carry = is_bit_set(32 - shift_v, value) };
-        case 32:
-            return (struct ShiftResult){ .value = 0, .carry = is_bit_set(0, value) };
-        default:
-            return (struct ShiftResult){ .value = 0, false };
+    if (shift_v == 0) {
+        return (struct ShiftResult){
+            .value = value,
+            .carry = old_carry
+        };
+    }
+    
+    else if (shift_v >= 1 && shift_v <= 31) {
+        return (struct ShiftResult){
+            .value = value << shift_v,
+            .carry = is_bit_set(32 - shift_v, value)
+        };
+    }
+    
+    else if (shift_v == 32) {
+        return (struct ShiftResult){
+            .value = 0,
+            .carry = is_bit_set(0, value)
+        };
+    }
+
+    else {
+        return (struct ShiftResult){
+            .value = 0,
+            false
+        };
     }
 }
 
 static inline struct ShiftResult barrel_shift_ROR(const uint32_t value, const uint32_t shift_v, const bool old_carry) {
-    switch (shift_v) {
-        case 0:
-            return (struct ShiftResult){ .value = value, .carry = old_carry };
-        case 1 ... 31:
-            return (struct ShiftResult){ .value = bit_rotr(shift_v, value), .carry = is_bit_set(shift_v - 1, value) };
-        case 32:
-            return (struct ShiftResult){ .value = value, .carry = is_bit_set(31, value) };
-        default: // todo: remove uneeded recursion
-            return barrel_shift_ROR(value, shift_v - 32, old_carry);
+    if (shift_v == 0) {
+        return (struct ShiftResult){
+            .value = value,
+            .carry = old_carry
+        };
+    }
+    
+    else if (shift_v >= 1 && shift_v <= 31) {
+        return (struct ShiftResult){
+            .value = bit_rotr(shift_v, value),
+            .carry = is_bit_set(shift_v - 1, value)
+        };
+    }
+    
+    else if (shift_v == 32) {
+        return (struct ShiftResult){
+            .value = value,
+            .carry = is_bit_set(31, value)
+        };
+    }
+
+    else {
+        // todo: remove uneeded recursion
+        return barrel_shift_ROR(value, shift_v - 32, old_carry);
     }
 }
 
